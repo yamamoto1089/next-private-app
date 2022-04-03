c609e39a28283271b439ad6ccbe83342bf3d487b
yamamotonaoyuki <yamamotonaoyuki@team-lab.com>
2022-04-04 02:31:09 +0900

    add: giojs用ページ追加

---
+++ pages/giojs/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Giojs: NextPage = () => {
+  return <div>Giojs</div>;
+};
+
+export default Giojs;
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,46 +18,18 @@ const Home: NextPage = () => {
         </h1>
 
         <div className={styles.description}>
-          <p>便利リンク集</p>
-          <span>
-            <a href="https://github.com/webhacck/js-play-2nd-edition">
-              【第2版】JavaScriptで徹底的に遊ぶ本 サポートサイト
-            </a>
-          </span>
+          <p>フロントのライブラリを簡易的に試すためのページ</p>
         </div>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="/giojs" className={styles.card}>
+            <h2>Gio.js &rarr;</h2>
+            <p>Gio.jsを使って遊んでみた</p>
           </a>
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -68,7 +40,7 @@ const Home: NextPage = () => {
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 };
